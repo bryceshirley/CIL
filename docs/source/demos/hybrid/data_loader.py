44db1ec6bf052e697ec5e4bb314078cbefbe28d3
bryceshirley <bryce.shirley@stfc.ac.uk>
2026-05-15 14:24:10 +0000

    feat+chores: add option to sphere data loader to load single slice or full 3D data and clean imports

--- docs/source/demos/hybrid/data_loader.py
+++ docs/source/demos/hybrid/data_loader.py
@@ -1,22 +1,23 @@
 # CIL core and io
-from cil.framework import ImageGeometry, AcquisitionGeometry, AcquisitionData
+from cil.framework import AcquisitionGeometry, AcquisitionData
 from cil.io import ZEISSDataReader
 from cil.io import TIFFStackReader
 
 # CIL processors
 from cil.processors import Binner, TransmissionAbsorptionConverter, Slicer
 
-# CIL optimisation algorithms and linear operators
+# CIL operators and algorithms
 from cil.recon import FDK
-from cil.processors import TransmissionAbsorptionConverter, Slicer
 from cil.plugins.astra import ProjectionOperator
 
-# CIL imports for data loading and visualisation
+# CIL Utilities
 from cil.utilities.display import show2D
+from cil.utilities import dataexample
 
 # Standard libraries
 import numpy as np
 import os
+import json
 
 
 def load_and_process_walnut(angle_step: int = 25, 
@@ -163,8 +164,6 @@ def load_and_process_cylinder(dataset_no: int=5, reduce_size: bool = True,
     proj_reader = TIFFStackReader(file_name=file_path)
     data = proj_reader.read()
 
-    import json
-
     # Read the JSON file
     with open(mi_file, "r") as jsonfile:
         d= json.load(jsonfile)
@@ -190,7 +189,6 @@ def load_and_process_cylinder(dataset_no: int=5, reduce_size: bool = True,
 
     ac_data.geometry.set_centre_of_rotation(-offset*pixel_size, angle=-tilt_angle_deg, angle_units='degree')
 
-    from cil.processors import TransmissionAbsorptionConverter
     ac_data = TransmissionAbsorptionConverter()(ac_data)
 
     # reorder data to match default order for Astra operator
@@ -219,15 +217,11 @@ def load_and_process_cylinder(dataset_no: int=5, reduce_size: bool = True,
         ig = ag.get_ImageGeometry()
         print(ig)
 
-
-    # Define projection operator
-    #A = ProjectionOperator(ig, ag, device = "gpu")
-
     # Check geometry
     print(ac_data)
     return ac_data, ig, ag
 
-def load_and_process_sphere(angle_step: int = 5):
+def load_and_process_sphere(angle_step: int = 5, single_slice: bool = False):
     """
     Loads and preprocesses the sphere dataset.
 
@@ -235,37 +229,62 @@ def load_and_process_sphere(angle_step: int = 5):
     ----------
     angle_step : int, optional
         The step size for angular subsampling. Default is 5.
+    single_slice : bool, optional
+        If True, extracts and processes only the central 2D slice. 
+        If False, processes the full 3D volume. Default is False.
 
     Returns
     -------
-    data : DataContainer
-        The processed CIL DataContainer.
+    absorption : DataContainer
+        The processed CIL DataContainer (absorption data).
     A : ProjectionOperator
         The Astra ProjectionOperator corresponding to the data geometry.
     ig : ImageGeometry
         The ImageGeometry of the dataset.
+    ground_truth : ImageData
+        The ground truth volume/slice.
+    recon : ImageData
+        The FDK reconstructed volume/slice.
     """
     
-    from cil.utilities import dataexample
     # Load data
     ground_truth = dataexample.SIMULATED_SPHERE_VOLUME.get()
-
     data = dataexample.SIMULATED_CONE_BEAM_DATA.get()
 
-    # Consider just a single 2D slice
-    data = data.get_slice(vertical='centre')
-    ground_truth = ground_truth.get_slice(vertical='centre')
+    # Conditionally extract a single 2D slice
+    if single_slice:
+        data = data.get_slice(vertical='centre')
+        ground_truth = ground_truth.get_slice(vertical='centre')
 
+    # Convert to absorption and subsample angles
     absorption = TransmissionAbsorptionConverter()(data)
-    absorption = Slicer(roi={'angle':(0, -1, angle_step)})(absorption)
-
-    ig = ground_truth.geometry
-
-    recon = FDK(absorption, image_geometry=ig).run()
-
-    show2D([ground_truth, recon], title = ['Ground Truth', 'FDK Reconstruction'], origin = 'upper', num_cols = 2)
-
-    A = ProjectionOperator(image_geometry=ig,
-                        acquisition_geometry=absorption.geometry)
+    absorption = Slicer(roi={'angle': (0, -1, angle_step)})(absorption)
     
-    return absorption, A, ig, ground_truth, recon
+    # Reconstruct the initial guess using TIGRE's FDK
+    absorption.reorder('tigre')
+    ig_tigre = ground_truth.geometry
+    recon = FDK(absorption, image_geometry=ig_tigre).run()
+
+    # Reorder data to match Astra toolbox
+    absorption.reorder('astra')
+    ground_truth.reorder('astra')
+    recon.reorder('astra')
+    
+    # Grab the updated geometry after reordering
+    ig_astra = ground_truth.geometry
+
+    # Visualization
+    if single_slice:
+        show2D([ground_truth, recon], 
+               title=['Ground Truth', 'FDK Reconstruction'], 
+               origin='upper', num_cols=2)
+    else:
+        # If 3D, grab the central slice just for the display plot
+        show2D([ground_truth.get_slice(vertical='centre'), recon.get_slice(vertical='centre')], 
+               title=['Ground Truth (Central Slice)', 'FDK Reconstruction (Central Slice)'], 
+               origin='upper', num_cols=2)
+
+    # Setup Astra Projection Operator
+    A = ProjectionOperator(image_geometry=ig_astra, acquisition_geometry=absorption.geometry)
+    
+    return absorption, A, ig_astra, ground_truth, recon
